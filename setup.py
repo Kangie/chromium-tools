45bddc1a45696feb9177cd9f850276a1b7d38744
Pawel Hajdan, Jr <phajdan.jr@gentoo.org>
2010-11-29 16:54:16 +0100

    Make subversion dependency optional.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import os.path
 import subprocess
+import sys
 
 from distutils.command.install_scripts import install_scripts
 from distutils.command.sdist import sdist
 from distutils.core import setup
+from distutils.errors import *
 
 def get_version_from_file():
 	return open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'VERSION')).read()
@@ -21,6 +23,23 @@ def get_version():
 		return get_version_from_file()
 	except IOError:
 		return get_version_from_git()
+
+def get_option(args, name, default=False):
+	disable_arg = "--disable-" + name
+	enable_arg = "--enable-" + name
+	if disable_arg in args and enable_arg in args:
+		raise DistutilsArgError(
+			"Conflicting flags: %s, %s" % (disable_arg, enable_arg))
+	
+	if disable_arg in args:
+		args.remove(disable_arg)
+		return False
+	
+	if enable_arg in args:
+		args.remove(enable_arg)
+		return True
+	
+	return default
 
 class my_sdist(sdist):
 	def make_release_tree(self, base_dir, files):
@@ -41,12 +60,21 @@ class my_install_scripts(install_scripts):
 		self.__symlink('chromium-depot-tool', 'gcl')
 		self.__symlink('chromium-depot-tool', 'gclient')
 
+scripts = ["v8-extract-version"]
+
+cmdclass = {'sdist': my_sdist}
+
+args = sys.argv[1:]
+
+enable_subversion = get_option(args, 'subversion', default=True)
+if enable_subversion:
+	scripts += ["chromium-depot-tool", "v8-create-tarball"]
+	cmdclass['install_scripts'] = my_install_scripts
+
 setup(
 	name="chromium-tools",
 	version=get_version(),
-	scripts=["chromium-depot-tool", "v8-create-tarball", "v8-extract-version"],
-	cmdclass={
-		'sdist': my_sdist,
-		'install_scripts': my_install_scripts,
-	}
+	scripts=scripts,
+	cmdclass=cmdclass,
+	script_args=args
 )
