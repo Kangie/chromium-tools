afa74fe53dfb6deacd24a9a91105882dfb5bec6b
Pawel Hajdan, Jr <phajdan.jr@gentoo.org>
2011-04-27 11:27:29 +0200

    Clean up:
    - remove switchable subversion support
    - remove unused v8-extract-version
    - take advantage of python-2.7 when it leads to simpler code
    - simplify setup.py
    - remove chromium_tools.py module (only one user now)

--- setup.py
+++ setup.py
@@ -12,10 +12,7 @@ def get_version_from_file():
 	return open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'VERSION')).read()
 
 def get_version_from_git():
-	# TODO: replace with check_output when python-2.7 is stable.
-	output = subprocess.Popen(
-		['git', 'describe'],
-		stdout=subprocess.PIPE).communicate()[0]
+	output = subprocess.check_output(['git', 'describe'])
 	chunks = output.split('-')
 	return '.'.join(chunks[:2])
 
@@ -24,23 +21,6 @@ def get_version():
 		return get_version_from_file()
 	except IOError:
 		return get_version_from_git()
-
-def get_option(args, name, default=False):
-	disable_arg = "--disable-" + name
-	enable_arg = "--enable-" + name
-	if disable_arg in args and enable_arg in args:
-		raise DistutilsArgError(
-			"Conflicting flags: %s, %s" % (disable_arg, enable_arg))
-	
-	if disable_arg in args:
-		args.remove(disable_arg)
-		return False
-	
-	if enable_arg in args:
-		args.remove(enable_arg)
-		return True
-	
-	return default
 
 class my_sdist(sdist):
 	def make_release_tree(self, base_dir, files):
@@ -55,25 +35,10 @@ class my_install_data(install_data):
 				os.path.join('..', 'libexec', 'chromium-depot-tool'),
 				os.path.join(self.install_dir, 'bin', tool))
 
-scripts = ["scripts/v8-extract-version"]
-data_files = []
-
-cmdclass = {'sdist': my_sdist}
-
-args = sys.argv[1:]
-
-enable_subversion = get_option(args, 'subversion', default=True)
-if enable_subversion:
-	scripts += ["scripts/v8-create-tarball"]
-	data_files += [["libexec", ["scripts/chromium-depot-tool"]]]
-	cmdclass['install_data'] = my_install_data
-
 setup(
 	name="chromium-tools",
 	version=get_version(),
-	py_modules=["chromium_tools"],
-	scripts=scripts,
-	data_files=data_files,
-	cmdclass=cmdclass,
-	script_args=args
+	scripts=["scripts/v8-create-tarball"],
+	data_files=[["libexec", ["scripts/chromium-depot-tool"]]],
+	cmdclass={'install_data': my_install_data, 'sdist': my_sdist},
 )
